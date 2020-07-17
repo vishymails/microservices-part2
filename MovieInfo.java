c8226d7b7dc9256aa189e4b25271df4b29d64984
vishymails <vishymails@gmail.com>
2020-07-17 17:06:36 +0530

    Add files via upload

---
+++ MovieInfo.java
@@ -0,0 +1,32 @@
+package com.ibm.moviecatalogservice.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.ibm.moviecatalogservice.models.CatalogItem;
+import com.ibm.moviecatalogservice.models.Movie;
+import com.ibm.moviecatalogservice.models.Rating;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@Service
+public class MovieInfo {
+
+	@Autowired
+    private RestTemplate restTemplate;
+
+	
+	 @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+		public CatalogItem getCatalogItem(Rating rating) {
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+			return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+		}
+	 
+	 
+	  public CatalogItem getFallbackCatalogItem(Rating rating) {
+	    	return new CatalogItem("Movie name not found", "", rating.getRating());
+	    }
+	    
+	  
+	  
+}
